ba990cb65caa7f916168edfd3413d8a87a3aab9a
Dharshinipa <126184056+Dharshinipa@users.noreply.github.com>
2023-08-24 22:57:49 +0530

    Add files via upload

---
+++ array11.java
@@ -0,0 +1,9 @@
+public class ArraySize {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        int size = array.length;
+
+        System.out.println("The number of elements in the array is: " + size);
+    }
+}
